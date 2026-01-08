169ba8797591084b6583f3ea7db46d1d01548a8c
EdAbreu01 <eddabreu@gmail.com>
2026-01-07 23:51:58 -0400

    Header done, beginning with the hero

--- uno.js
+++ uno.js
@@ -1,8 +1,11 @@
-let uno = [2,4,6,3,2]
+let suma = 0;
+const uno = [2,4,65,2,3]
+uno.push(4)
 
-for (let i = 0; i < uno.length;i++){
-    let result = uno[i] ++
-    console.log(result)
-    
+for (let i = 0; i < uno.length; i++){
+    suma += uno[i]
 }
 
+    console.log(`El array tiene ${uno.length} elementos`)
+
+    console.log(`Todos sumados hacen ${suma}`)
