c2c1672e1185b29d1e9f5f013388f05ccd5c8d7b
EdAbreu01 <eddabreu@gmail.com>
2026-01-11 00:32:33 -0400

    beginning with the footer section!

--- uno.js
+++ uno.js
@@ -2,7 +2,5 @@ let suma = 0;
 
 const numeros = [2,4,5,11,2]
 
-for (let i = 0; i < numeros.length;i++){
-    suma += numeros[i]
-}
-    console.log(suma)
+
+    console.log(numeros[numeros.length-1])
